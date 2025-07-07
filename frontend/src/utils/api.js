1d1e794fc8cee46e9dd2796b315247469f7879f9
Ay0u8dev <ayoubelebbar09@gmail.com>
2025-07-07 13:24:37 +0100

    a

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromCookie } from "./AuthProvider";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
