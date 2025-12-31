d7b21be1e444985a5fcce6539b7ce3643cc638c4
ismail <ismailbouras1968@gmail.com>
2025-12-31 14:40:39 +0100

    ee

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,49 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { ThemeProvider } from "./components/theme-provider";
-import { Routes, Route } from "react-router-dom";
-import { Navigate, useNavigate } from "react-router-dom";
-import { Toaster } from "sonner";
-import {
-  useAuth,
-  getInfoFromToken,
-  getTokenFromCookie,
-} from "./utils/AuthProvider";
-import RoleViewResolver from "./utils/RoleViewResolver";
+"use client"
 
-import LoginPage from "./vues/LoginPage";
-import MainPage from "./vues/MainPage";
-import ProtectedRoute from "./utils/ProtectedRoute";
+import { useEffect, useState } from "react"
+import "./App.css"
+import { ThemeProvider } from "./components/theme-provider"
+import { Routes, Route } from "react-router-dom"
+import { Navigate, useNavigate } from "react-router-dom"
+import { Toaster } from "sonner"
+import { useAuth, getInfoFromToken, getTokenFromCookie } from "./utils/AuthProvider"
+import RoleViewResolver from "./utils/RoleViewResolver"
 
-import GMMainPage from "./gestion_marche/vues/MainPage";
-import MarchesPage from "./gestion_marche/vues/marches/page";
-import MarchesPerIdPage from "./gestion_marche/vues/marches/id/page";
-import BDPage from "./gestion_marche/vues/bons-commande/page";
-import BDPerIdPage from "./gestion_marche/vues/bons-commande/id/page";
-import ConractPage from "./gestion_marche/vues/contrats/page";
-import ContractPerIdPage from "./gestion_marche/vues/contrats/id/page";
-import ParametrageGMPage from "./gestion_marche/vues/parametrage/page";
-import GMDashboardPage from "./gestion_marche/vues/tableau-de-bord/page";
-import AOPage from "./gestion_marche/vues/appel-offre/page";
-import AOByIdPage from "./gestion_marche/vues/appel-offre/id/page";
+import LoginPage from "./vues/LoginPage"
+import MainPage from "./vues/MainPage"
+import ProtectedRoute from "./utils/ProtectedRoute"
 
-import ParcAutoLayout from "./gestion_parc_auto/pages/ParcAutoLayout";
-import VehiculesList from "./gestion_parc_auto/pages/VehiculesList";
-import VehiculeForm from "./gestion_parc_auto/pages/VehiculeForm";
-import VehiculeEditForm from "./gestion_parc_auto/pages/VehiculeEditForm";
-import ChauffeursList from "./gestion_parc_auto/pages/ChauffeursList";
-import ChauffeurForm from "./gestion_parc_auto/pages/ChauffeurForm";
-import ChauffeurEditForm from "./gestion_parc_auto/pages/ChauffeurEditForm";
-import MissionsList from "./gestion_parc_auto/pages/MissionsList";
-import MissionForm from "./gestion_parc_auto/pages/MissionForm";
-import MissionEditForm from "./gestion_parc_auto/pages/MissionEditForm";
-import { NotificationProvider } from "./utils/NotificationProvider";
-import NotificationsPage from "./gestion_parc_auto/pages/NotificationsPage";
+import GMMainPage from "./gestion_marche/vues/MainPage"
+import MarchesPage from "./gestion_marche/vues/marches/page"
+import MarchesPerIdPage from "./gestion_marche/vues/marches/id/page"
+import BDPage from "./gestion_marche/vues/bons-commande/page"
+import BDPerIdPage from "./gestion_marche/vues/bons-commande/id/page"
+import ConractPage from "./gestion_marche/vues/contrats/page"
+import ContractPerIdPage from "./gestion_marche/vues/contrats/id/page"
+import ParametrageGMPage from "./gestion_marche/vues/parametrage/page"
+import GMDashboardPage from "./gestion_marche/vues/tableau-de-bord/page"
+import AOPage from "./gestion_marche/vues/appel-offre/page"
+import AOByIdPage from "./gestion_marche/vues/appel-offre/id/page"
 
-import DepensesList from "./gestion_parc_auto/pages/DepensesList";
-import MoyensPaiementList from "./gestion_parc_auto/pages/MoyensPaiementList";
-import DepenseForm from "./gestion_parc_auto/pages/DepenseForm";
-import MoyenPaiementForm from "./gestion_parc_auto/pages/MoyenPaiementForm";
-import DepenseEditForm from "./gestion_parc_auto/pages/DepenseEditForm";
-import MoyenPaiementEditForm from "./gestion_parc_auto/pages/MoyenPaiementEditForm";
+import ParcAutoLayout from "./gestion_parc_auto/pages/ParcAutoLayout"
+import VehiculesList from "./gestion_parc_auto/pages/VehiculesList"
+import VehiculeForm from "./gestion_parc_auto/pages/VehiculeForm"
+import VehiculeEditForm from "./gestion_parc_auto/pages/VehiculeEditForm"
+import ChauffeursList from "./gestion_parc_auto/pages/ChauffeursList"
+import ChauffeurForm from "./gestion_parc_auto/pages/ChauffeurForm"
+import ChauffeurEditForm from "./gestion_parc_auto/pages/ChauffeurEditForm"
+import MissionsList from "./gestion_parc_auto/pages/MissionsList"
+import MissionForm from "./gestion_parc_auto/pages/MissionForm"
+import MissionEditForm from "./gestion_parc_auto/pages/MissionEditForm"
+import { NotificationProvider } from "./utils/NotificationProvider"
+import NotificationsPage from "./gestion_parc_auto/pages/NotificationsPage"
+
+import DepensesList from "./gestion_parc_auto/pages/DepensesList"
+import MoyensPaiementList from "./gestion_parc_auto/pages/MoyensPaiementList"
+import DepenseForm from "./gestion_parc_auto/pages/DepenseForm"
+import MoyenPaiementForm from "./gestion_parc_auto/pages/MoyenPaiementForm"
+import DepenseEditForm from "./gestion_parc_auto/pages/DepenseEditForm"
+import MoyenPaiementEditForm from "./gestion_parc_auto/pages/MoyenPaiementEditForm"
 
 function gestionDemandeRoutes() {
   return (
@@ -53,9 +51,7 @@ function gestionDemandeRoutes() {
       <Route
         path="/md/dashboard"
         element={
-          <ProtectedRoute
-            role={["ROLE_ADMIN", "ROLE_INFO", "ROLE_LOGISTICS", "ROLE_USER"]}
-          >
+          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_INFO", "ROLE_LOGISTICS", "ROLE_USER"]}>
             <RoleViewResolver page="dashboard" />
           </ProtectedRoute>
         }
@@ -118,15 +114,13 @@ function gestionDemandeRoutes() {
       <Route
         path="/md/*"
         element={
-          <ProtectedRoute
-            role={["ROLE_USER", "ROLE_INFO", "ROLE_LOGISTICS", "ROLE_ADMIN"]}
-          >
+          <ProtectedRoute role={["ROLE_USER", "ROLE_INFO", "ROLE_LOGISTICS", "ROLE_ADMIN"]}>
             <Navigate to="/md/dashboard" />
           </ProtectedRoute>
         }
       />
     </>
-  );
+  )
 }
 
 function gestionMarcheRoutes() {
@@ -135,7 +129,7 @@ function gestionMarcheRoutes() {
       <Route
         path="/gm"
         element={
-          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS","ROLE_DIR"]}>
+          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS", "ROLE_DIR"]}>
             <GMMainPage />
           </ProtectedRoute>
         }
@@ -144,7 +138,7 @@ function gestionMarcheRoutes() {
       <Route
         path="/gm/appelOffres"
         element={
-          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS","ROLE_DIR"]}>
+          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS", "ROLE_DIR"]}>
             <AOPage />
           </ProtectedRoute>
         }
@@ -152,7 +146,7 @@ function gestionMarcheRoutes() {
       <Route
         path="/gm/appelOffres/:id"
         element={
-          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS","ROLE_DIR"]}>
+          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS", "ROLE_DIR"]}>
             <AOByIdPage />
           </ProtectedRoute>
         }
@@ -160,7 +154,7 @@ function gestionMarcheRoutes() {
       <Route
         path="/gm/marches"
         element={
-          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS","ROLE_DIR"]}>
+          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS", "ROLE_DIR"]}>
             <MarchesPage />
           </ProtectedRoute>
         }
@@ -168,7 +162,7 @@ function gestionMarcheRoutes() {
       <Route
         path="/gm/marches/:id"
         element={
-          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS","ROLE_DIR"]}>
+          <ProtectedRoute role={["ROLE_ADMIN", "ROLE_LOGISTICS", "ROLE_DIR"]}>
             <MarchesPerIdPage />
           </ProtectedRoute>
         }
@@ -231,7 +225,7 @@ function gestionMarcheRoutes() {
         }
       />
     </>
-  );
+  )
 }
 
 function gestionParcAutoRoutes() {
@@ -249,62 +243,44 @@ function gestionParcAutoRoutes() {
       {/* Redirige /parc-auto vers /parc-auto/vehicules */}
       <Route path="vehicules" element={<VehiculesList />} />
       <Route path="vehicules/ajouter" element={<VehiculeForm />} />
-      <Route
-        path="vehicules/modifier/:vehiculeId"
-        element={<VehiculeEditForm />}
-      />
+      <Route path="vehicules/modifier/:vehiculeId" element={<VehiculeEditForm />} />
       <Route path="chauffeurs" element={<ChauffeursList />} />
       <Route path="chauffeurs/ajouter" element={<ChauffeurForm />} />
-      <Route
-        path="chauffeurs/modifier/:chauffeurId"
-        element={<ChauffeurEditForm />}
-      />
+      <Route path="chauffeurs/modifier/:chauffeurId" element={<ChauffeurEditForm />} />
       <Route path="missions" element={<MissionsList />} />
       <Route path="missions/ajouter" element={<MissionForm />} />
-      <Route
-        path="missions/modifier/:missionId"
-        element={<MissionEditForm />}
-      />
+      <Route path="missions/modifier/:missionId" element={<MissionEditForm />} />
       <Route path="notifications" element={<NotificationsPage />} />
       <Route path="depenses" element={<DepensesList />} />
       <Route path="moyens-paiement" element={<MoyensPaiementList />} />
       <Route path="depenses/ajouter" element={<DepenseForm />} />
       <Route path="moyens-paiement/ajouter" element={<MoyenPaiementForm />} />
-      <Route
-        path="moyens-paiement/modifier/:moyenPaiementId"
-        element={<MoyenPaiementEditForm />}
-      />
-      <Route
-        path="depenses/modifier/:depenseId"
-        element={<DepenseEditForm />}
-      />
-      <Route
-        path="*"
-        element={<Navigate to="/parc-auto/vehicules" replace />}
-      />
+      <Route path="moyens-paiement/modifier/:moyenPaiementId" element={<MoyenPaiementEditForm />} />
+      <Route path="depenses/modifier/:depenseId" element={<DepenseEditForm />} />
+      <Route path="*" element={<Navigate to="/parc-auto/vehicules" replace />} />
     </Route>
-  );
+  )
 }
 
 function App() {
-  const { userDetails, login } = useAuth();
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
+  const { userDetails, login } = useAuth()
+  const [loading, setLoading] = useState(true)
+  const navigate = useNavigate()
 
   useEffect(() => {
-    const userInfo = getInfoFromToken(getTokenFromCookie());
+    const userInfo = getInfoFromToken(getTokenFromCookie())
     if (userDetails === null) {
       if (userInfo) {
-        console.log("User info form App.jsx", userInfo);
-        login(userInfo);
+        console.log("User info form App.jsx", userInfo)
+        login(userInfo)
       }
     }
 
-    setLoading(false);
-  }, []);
+    setLoading(false)
+  }, [])
 
   if (loading) {
-    return null;
+    return null
   }
 
   return (
@@ -316,10 +292,10 @@ function App() {
             <Route
               path="/"
               element={
-                userDetails &&
-                (userDetails?.role == "ROLE_ADMIN" ||
-                  userDetails?.role == "ROLE_LOGISTICS") ? (
+                userDetails && (userDetails?.role == "ROLE_ADMIN" || userDetails?.role == "ROLE_LOGISTICS") ? (
                   <MainPage />
+                ) : userDetails?.role == "ROLE_DIR" ? (
+                  <Navigate to="/gm" />
                 ) : (
                   <Navigate to="/md/dashboard" />
                 )
@@ -338,16 +314,10 @@ function App() {
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </NotificationProvider>
-        <Toaster
-          richColors
-          visibleToasts={1}
-          duration={2000}
-          position="top-center"
-          style={{ marginTop: "1rem" }}
-        />
+        <Toaster richColors visibleToasts={1} duration={2000} position="top-center" style={{ marginTop: "1rem" }} />
       </ThemeProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
